cd2c81c3338cd0db709d70f14c46f430d41e5a90
Sumit Rathore <sumit@neolen.com>
2023-05-22 16:44:09 +0530

    gitignore added.

--- ezbzrental/settings.py
+++ ezbzrental/settings.py
@@ -91,15 +91,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ezbz',
-        'USER': 'diwaker',#django
-        # 'USER': 'django',
-        #'USER': 'root',
-        'PASSWORD': 'diwaker', #India@2023
-        # 'PASSWORD': 'India@2023',
-        #'PASSWORD': 'Mysql@2022',
+        'USER': 'django',
+        # 'USER': 'root',
+        'PASSWORD': 'India@2023',
+        # 'PASSWORD': 'Mysql@2022',
         'HOST': 'localhost',
-        'PORT': '3306', 
-        
+        'PORT': '3306',
+
     }
 }
 
